a7b940d30460809a09a7a4fee2e3152b41187358
konifar <yahpeycoy0403@gmail.com>
2016-08-14 15:22:34 +0900

    [Add] Insert string key to editor :memo:

--- src/main/java/com/konifar/stringssearch/SearchStringsAction.java
+++ src/main/java/com/konifar/stringssearch/SearchStringsAction.java
@@ -6,6 +6,11 @@ import com.intellij.ide.util.gotoByName.ChooseByNameFilter;
 import com.intellij.ide.util.gotoByName.ChooseByNamePopup;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.command.CommandProcessor;
+import com.intellij.openapi.command.UndoConfirmationPolicy;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import main.java.com.konifar.stringssearch.models.StringElement;
@@ -13,6 +18,8 @@ import main.java.com.konifar.stringssearch.search.SearchStringFilterFactory;
 import main.java.com.konifar.stringssearch.search.SearchStringItemProvider;
 import main.java.com.konifar.stringssearch.search.SearchStringModel;
 import org.jetbrains.annotations.NotNull;
+
+import static com.intellij.openapi.application.ApplicationManager.getApplication;
 
 public class SearchStringsAction extends GotoActionBase implements DumbAware {
 
@@ -32,8 +39,9 @@ public class SearchStringsAction extends GotoActionBase implements DumbAware {
 
             @Override
             public void elementChosen(ChooseByNamePopup popup, Object element) {
-                if (element == null) return;
-                // TODO
+                if (element != null && element instanceof StringElement) {
+                    insertToEditor(project, (StringElement) element);
+                }
             }
         };
 
@@ -41,4 +49,26 @@ public class SearchStringsAction extends GotoActionBase implements DumbAware {
         showNavigationPopup(e, searchStringModel, callback, "strings matching pattern", true, true, provider);
     }
 
+    private void insertToEditor(final Project project, final StringElement stringElement) {
+        CommandProcessor.getInstance().executeCommand(project, new Runnable() {
+            @Override
+            public void run() {
+                getApplication().runWriteAction(new Runnable() {
+                    @Override
+                    public void run() {
+                        Editor editor = FileEditorManager.getInstance(project).getSelectedTextEditor();
+                        if (editor != null) {
+                            int offset = editor.getCaretModel().getOffset();
+                            Document document = editor.getDocument();
+                            String key = stringElement.getName();
+                            if (key != null) {
+                                document.insertString(offset, key);
+                                editor.getCaretModel().moveToOffset(offset + key.length());
+                            }
+                        }
+                    }
+                });
+            }
+        }, "WriteStringKeyCommand", "", UndoConfirmationPolicy.DO_NOT_REQUEST_CONFIRMATION);
+    }
 }
