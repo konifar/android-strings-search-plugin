8445b04777e7808f20f2ea592d2b2114f874b037
konifar <yahpeycoy0403@gmail.com>
2016-08-14 10:49:04 +0900

    [Modify] Refactored renderer :art:

--- src/main/java/com/konifar/stringssearch/search/StringElementListCellRenderer.java
+++ src/main/java/com/konifar/stringssearch/search/StringElementListCellRenderer.java
@@ -1,30 +1,18 @@
 package main.java.com.konifar.stringssearch.search;
 
 import com.intellij.icons.AllIcons;
-import com.intellij.navigation.ColoredItemPresentation;
-import com.intellij.navigation.ItemPresentation;
-import com.intellij.navigation.NavigationItem;
-import com.intellij.openapi.editor.colors.EditorColorsManager;
-import com.intellij.openapi.editor.colors.TextAttributesKey;
-import com.intellij.openapi.editor.markup.TextAttributes;
-import com.intellij.openapi.ui.popup.PopupChooserBuilder;
-import com.intellij.openapi.util.Iconable;
-import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.ui.ColoredListCellRenderer;
 import com.intellij.ui.JBColor;
 import com.intellij.ui.SimpleTextAttributes;
 import com.intellij.ui.speedSearch.SpeedSearchUtil;
-import com.intellij.util.Function;
-import com.intellij.util.IconUtil;
 import com.intellij.util.text.Matcher;
 import com.intellij.util.text.MatcherHolder;
 import com.intellij.util.ui.UIUtil;
+import icons.AndroidIcons;
 import main.java.com.konifar.stringssearch.models.StringElement;
-import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.Comparator;
 
 /**
  * @see com.intellij.ide.util.PsiElementListCellRenderer
@@ -34,8 +22,6 @@ final class StringElementListCellRenderer extends JPanel implements ListCellRend
     private int maxWidth;
 
     private Matcher matcher;
-
-    private boolean focusBorderEnabled = true;
 
     StringElementListCellRenderer(int maxWidth) {
         super(new BorderLayout());
@@ -51,158 +37,59 @@ final class StringElementListCellRenderer extends JPanel implements ListCellRend
     public Component getListCellRendererComponent(JList list, Object value, int index,
                                                   boolean isSelected, boolean cellHasFocus) {
         removeAll();
+
+        // Right
         DefaultListCellRenderer rightRenderer = new RightCellRenderer();
-        Component rightCellRendererComponent = rightRenderer.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
-        add(rightCellRendererComponent, BorderLayout.EAST);
+        Component rightComponent = rightRenderer.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+        add(rightComponent, BorderLayout.EAST);
+
+        // Space
         JPanel spacer = new JPanel();
         spacer.setBorder(BorderFactory.createEmptyBorder(0, 2, 0, 2));
         add(spacer, BorderLayout.CENTER);
 
-        ListCellRenderer leftRenderer = new LeftRenderer(null, matcher);
-        final Component leftCellRendererComponent = leftRenderer.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
-        add(leftCellRendererComponent, BorderLayout.WEST);
-        final Color bg = isSelected ? UIUtil.getListSelectionBackground() : leftCellRendererComponent.getBackground();
+        // Left
+        ListCellRenderer leftRenderer = new LeftCellRenderer(matcher);
+        Component leftComponent = leftRenderer.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+        add(leftComponent, BorderLayout.WEST);
+
+        // Background color
+        Color bg = isSelected ? UIUtil.getListSelectionBackground() : leftComponent.getBackground();
         setBackground(bg);
-        if (rightCellRendererComponent != null) {
-            rightCellRendererComponent.setBackground(bg);
-        }
-        if (spacer != null) {
-            spacer.setBackground(bg);
-        }
+        rightComponent.setBackground(bg);
+        spacer.setBackground(bg);
+
         return this;
     }
 
-    private class LeftRenderer extends ColoredListCellRenderer {
-        private final String moduleName;
+    private class LeftCellRenderer extends ColoredListCellRenderer {
         private final Matcher matcher;
 
-        public LeftRenderer(final String moduleName, Matcher matcher) {
-            this.moduleName = moduleName;
+        private LeftCellRenderer(Matcher matcher) {
             this.matcher = matcher;
         }
 
         @Override
         protected void customizeCellRenderer(JList list, Object value, int index, boolean selected, boolean hasFocus) {
             Color bgColor = UIUtil.getListBackground();
-            Color color = list.getForeground();
-            setPaintFocusBorder(hasFocus && UIUtil.isToUseDottedCellBorder() && focusBorderEnabled);
+            setPaintFocusBorder(hasFocus && UIUtil.isToUseDottedCellBorder());
+
             if (value instanceof StringElement) {
                 StringElement element = (StringElement) value;
                 String name = element.getValue();
 
-                TextAttributes attributes = getNavigationItemAttributes(value);
-
-                SimpleTextAttributes nameAttributes = attributes != null
-                        ? SimpleTextAttributes.fromTextAttributes(attributes)
-                        : null;
-
-                if (nameAttributes == null) {
-                    nameAttributes = new SimpleTextAttributes(Font.PLAIN, color);
-                }
-
-                assert name != null : "Null name for PSI element " + element + " (by " + StringElementListCellRenderer.this + ")";
+                SimpleTextAttributes nameAttributes = new SimpleTextAttributes(Font.PLAIN, list.getForeground());
                 SpeedSearchUtil.appendColoredFragmentForMatcher(name, this, nameAttributes, matcher, bgColor, selected);
-//                setIcon(StringElementListCellRenderer.this.getIcon(element));
+                // TODO Change icon
+                setIcon(AndroidIcons.EmptyFlag);
 
-                String containerText = getContainerTextForLeftComponent(element, name + (moduleName != null ? moduleName + "        " : ""));
-                if (containerText != null) {
-                    append(" " + containerText, new SimpleTextAttributes(Font.PLAIN, JBColor.GRAY));
-                }
-            } else if (!customizeNonPsiElementLeftRenderer(this, list, value, index, selected, hasFocus)) {
-                setIcon(IconUtil.getEmptyIcon(false));
-                append(value == null ? "" : value.toString(), new SimpleTextAttributes(Font.PLAIN, list.getForeground()));
+                String stringKeyText = "(" + element.getName() + ")";
+                append(" " + stringKeyText, new SimpleTextAttributes(Font.PLAIN, JBColor.GRAY));
             }
+
             setBackground(selected ? UIUtil.getListSelectionBackground() : bgColor);
         }
 
-    }
-
-    @Nullable
-    private TextAttributes getNavigationItemAttributes(Object value) {
-        TextAttributes attributes = null;
-
-        if (value instanceof NavigationItem) {
-            TextAttributesKey attributesKey = null;
-            final ItemPresentation presentation = ((NavigationItem) value).getPresentation();
-            if (presentation instanceof ColoredItemPresentation) attributesKey =
-                    ((ColoredItemPresentation) presentation).getTextAttributesKey();
-
-            if (attributesKey != null) {
-                attributes = EditorColorsManager.getInstance().getGlobalScheme().getAttributes(attributesKey);
-            }
-        }
-        return attributes;
-    }
-
-    @Nullable
-    protected String getContainerTextForLeftComponent(StringElement element, final String name) {
-        return "(" + element.getName() + ")";
-    }
-
-    protected boolean customizeNonPsiElementLeftRenderer(ColoredListCellRenderer renderer,
-                                                         JList list,
-                                                         Object value,
-                                                         int index,
-                                                         boolean selected,
-                                                         boolean hasFocus) {
-        if (!(value instanceof NavigationItem)) return false;
-
-        NavigationItem item = (NavigationItem) value;
-
-        TextAttributes attributes = getNavigationItemAttributes(item);
-
-        SimpleTextAttributes nameAttributes = attributes != null ? SimpleTextAttributes.fromTextAttributes(attributes) : null;
-
-        Color color = list.getForeground();
-        if (nameAttributes == null) nameAttributes = new SimpleTextAttributes(Font.PLAIN, color);
-
-        renderer.append(item + " ", nameAttributes);
-        ItemPresentation itemPresentation = item.getPresentation();
-        assert itemPresentation != null;
-        renderer.setIcon(itemPresentation.getIcon(true));
-
-        String locationString = itemPresentation.getLocationString();
-        if (!StringUtil.isEmpty(locationString)) {
-            renderer.append(locationString, new SimpleTextAttributes(Font.PLAIN, JBColor.GRAY));
-        }
-        return true;
-    }
-
-    protected void setFocusBorderEnabled(boolean enabled) {
-        focusBorderEnabled = enabled;
-    }
-
-    public Comparator<StringElement> getComparator() {
-        return new Comparator<StringElement>() {
-            @Override
-            public int compare(StringElement o1, StringElement o2) {
-                return o1.getValue().compareTo(o2.getValue());
-            }
-        };
-    }
-
-    public void installSpeedSearch(PopupChooserBuilder builder) {
-        installSpeedSearch(builder, false);
-    }
-
-    public void installSpeedSearch(PopupChooserBuilder builder, final boolean includeContainerText) {
-        builder.setFilteringEnabled(new Function<Object, String>() {
-            @Override
-            public String fun(Object o) {
-                if (o instanceof StringElement) {
-                    if (includeContainerText) {
-                        return ((StringElement) o).getValue();
-                    }
-                    return ((StringElement) o).getValue();
-                } else {
-                    return o.toString();
-                }
-            }
-        });
-    }
-
-    protected int getIconFlags() {
-        return Iconable.ICON_FLAG_READ_STATUS;
     }
 
     private class RightCellRenderer extends DefaultListCellRenderer {
@@ -213,15 +100,16 @@ final class StringElementListCellRenderer extends JPanel implements ListCellRend
 
         public Component getListCellRendererComponent(JList list, Object value, int index,
                                                       boolean isSelected, boolean cellHasFocus) {
-            Component listCellRendererComponent = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
-            customizeCellRenderer(value, isSelected);
-            return listCellRendererComponent;
+            Component component = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+            customize(value, isSelected);
+            return component;
         }
 
-        private void customizeCellRenderer(Object value, boolean selected) {
+        private void customize(Object value, boolean selected) {
             if (value instanceof StringElement) {
                 StringElement element = (StringElement) value;
 
+                // TODO Change icon to each countries flags.
                 setIcon(AllIcons.Modules.SourceFolder);
                 setText(element.getParentDirName());
 
